cd44e9889b055986ba0939d16a884cf17b495813
Catherine Tan <llt3@sfu.ca>
2013-02-16 22:50:34 -0800

    refactor project

---
+++ MutuiTuition/src/com/coppio/tuition/LoanUtils.java
@@ -0,0 +1,23 @@
+package com.coppio.tuition;
+
+import java.math.BigDecimal;
+
+
+public class LoanUtils {
+	
+	public static BigDecimal calculateMonthlyPayment(BigDecimal principal, BigDecimal term, BigDecimal interest)
+	{
+		return new BigDecimal("0");
+	}
+	
+	public static BigDecimal calculateNet(BigDecimal principal, BigDecimal term, BigDecimal interest)
+	{
+		
+		return new BigDecimal("0");
+	}
+	public static BigDecimal calculateTotalInterest(BigDecimal principal, BigDecimal term, BigDecimal interest)
+	{
+		
+		return new BigDecimal("0");
+	}
+}
---
+++ MutuiTuition/src/com/coppio/tuition/MutuiTuition.java
@@ -0,0 +1,18 @@
+package com.coppio.tuition;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.corgipark.mutui.car.R;
+
+public class MutuiTuition extends Activity  {
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+
+}
